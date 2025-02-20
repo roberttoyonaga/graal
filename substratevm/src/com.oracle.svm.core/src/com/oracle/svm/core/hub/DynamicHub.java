f2e83d4311d3966a49dd3fe83933f5a23e2ae4a5
Robert Toyonaga <rtoyonag@redhat.com>
2025-02-20 16:35:45 -0500

    allocation free

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/hub/DynamicHub.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/hub/DynamicHub.java
@@ -1000,7 +1000,7 @@ public final class DynamicHub implements AnnotatedElement, java.lang.reflect.Typ
     }
 
     @Substitute
-    private DynamicHub getComponentType() {
+    public DynamicHub getComponentType() {
         return componentType;
     }
 
