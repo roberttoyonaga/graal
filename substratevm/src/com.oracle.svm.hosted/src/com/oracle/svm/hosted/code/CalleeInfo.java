433295e42f7b5a163751f2b1380bc8d7eb333dd0
Robert Toyonaga <rtoyonag@redhat.com>
2025-10-08 15:43:22 -0400

    weight specific canon differently. Multiple callsites. Fix sizebeforeInlining bug

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CalleeInfo.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CalleeInfo.java
@@ -3,12 +3,17 @@ package com.oracle.svm.hosted.code;
 public class CalleeInfo {
     public double bc;
     public int depth;
+    public int sizeBeforeInlining; // This depends on the root graph, not the callee at all.
+    public int lastRoundUpdated; // This is needed for dealing with multiple callsites.
     public com.oracle.svm.hosted.meta.HostedMethod method;
-    public boolean ignore;
-    public CalleeInfo (double bc, int depth, com.oracle.svm.hosted.meta.HostedMethod method) {
+    public boolean ignore; // We've already evaluated this callee for inlining and decided it wasn't worth it.
+    public boolean secondLevel;
+    public CalleeInfo (double bc, int depth, com.oracle.svm.hosted.meta.HostedMethod method, boolean secondLevel, int lastRoundUpdated) {
         this.bc = bc;
         this.depth = depth;
         this.method = method;
+        this.secondLevel = secondLevel;
+        this.lastRoundUpdated = lastRoundUpdated;
 
     }
 }
